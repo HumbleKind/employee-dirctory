c91cda30f24b47a9528c37cdacbe167a7b889efa
Christian Sinai <christian_sinai@me.com>
2020-12-01 14:45:42 -0800

    added search form/ filstered event handler

--- src/components/Directory.js
+++ src/components/Directory.js
@@ -1,24 +1,22 @@
-// QUESTION: by 'destructuring'? here, does that make it possible NOT to use extends "React.Component" in the class creation?
 import React, { Component } from "react";
 import Table from 'react-bootstrap/Table';
 
 // import API from "../utils/API";
 
-// QUESTION: is it best practice to use class instead of functional components?
 class Directory extends Component {
-    // QUESTION: is it best practice to use constructor/ super when designating 'state'?
     constructor(props) {
-        // QUESTION: why is "super" crossed out? Is it OK to remove "props" from super (and constructor)?
         super(props);
         this.state = {
             error: null,
             isLoaded: false,
+            search: "",
             results: []
         };
       }
 
     componentDidMount() {
         fetch("https://randomuser.me/api/?results=200&nat=us")
+        // API.getUsers()
             .then(res => res.json())
             .then(
                 (result) => {
@@ -36,6 +34,27 @@ class Directory extends Component {
             )
     }
 
+    handleInputChange = event => {
+
+        const value = event.target.value;
+        const name = event.target.name;
+
+        const filteredUsers = this.state.results.filter(user => {
+            let userName = user.name.first + user.name.last;
+            userName = userName.toLowerCase();
+            console.log(userName);
+            
+            return userName.includes(value);
+        });
+    
+        this.setState({
+            [name]: value,
+            results: filteredUsers
+        });
+
+        console.log(this.state.search);
+    };
+
     render() {
         const { error, isLoaded, results } = this.state;
         if (error) {
@@ -45,7 +64,15 @@ class Directory extends Component {
         } else {
             return (
                 <div>
-                    {/* how do I use these props correctly? */}
+                    <form className="form">
+                        <input
+                            name="search"
+                            onChange={this.handleInputChange}
+                            type="text"
+                            placeholder="Search"
+                        />
+                        {/* <button onClick={this.handleFormSubmit}>Submit</button> */}
+                    </form>
                     <Table striped bordered hover>
                         <thead>
                             <tr>
@@ -58,7 +85,6 @@ class Directory extends Component {
                         </thead>
                         <tbody>
                             {results.map(employee => (
-                                // what's the bes 'key' to use, and why?
                                 <tr key={employee.id.value}>
                                     <td><img src={employee.picture.thumbnail} alt="" /></td>
                                     <td>{employee.name.first} {employee.name.last}</td>
